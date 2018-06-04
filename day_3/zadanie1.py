daaf4b3d2416de80ee397db6c325779adaac2eed
Przemyslaw Lalak <lalak@protonmail.com>
2018-06-04 20:56:40 +0200

    normalize user's input

--- day_3/zadanie1.py
+++ day_3/zadanie1.py
@@ -1,8 +1,11 @@
 ADMIN_USERNAME = 'Piotr'
 name = input('Podaj imie: ')
 
-if name == ADMIN_USERNAME:
+name_capitalized = name.lower().capitalize()
+
+if name_capitalized == ADMIN_USERNAME:
     print('Hey', name)
-    # print(f'Hey {name}')
 else:
     print('Hey Poklikash?!')
+
+# TODO: some comment
