651c2eab79974fd9e602edb106a662239806f8df
Przemyslaw Lalak <lalak@protonmail.com>
2018-06-04 21:18:49 +0200

    check whether value is even or odd

---
+++ day_3/zadanie2.py
@@ -0,0 +1,9 @@
+# TODO: napisz program ktory sprawdzi czy liczba jest parzysta
+# TODO: liczba do sprawdzenia jest podawana przez uzyszkodnika
+
+value = int(input('Podaj liczbe: '))
+
+if value % 2:
+    print('Nieparzysta')
+else:
+    print('Parzysta')
